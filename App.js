7ffeb255a633d2ddbc131196b58fc8301f896a88
Administrator <Administrator@kohasaeng.local>
2021-03-24 09:59:09 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import AddEntry from './components/AddEntry';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import History from './components/History';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	return (
+		<Provider store={createStore(reducer)}>
+			<View style={{ flex: 1 }}>
+				<View style={{ height: 20 }} />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+				<History />
+			</View>
+		</Provider>
+	);
+}
